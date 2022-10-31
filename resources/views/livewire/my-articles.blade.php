1976d12da842ecb6c6e6547f65631c99852c92d9
Afonso247 <afonso.h2002@gmail.com>
2022-10-31 15:38:53 -0300

    Preparando o restante do CRUD de artigos

--- resources/views/livewire/my-articles.blade.php
+++ resources/views/livewire/my-articles.blade.php
@@ -1,22 +1,42 @@
-<div>
-    <p>Mostrando últimos artigos</p>
+@section('web-title', 'Meus artigos')
 
-    <input type="text" name="message" id="message" wire:model="message">
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('Meus artigos') }}.
+    </h2>
+</x-slot>
 
-    <hr>
-
-    
-    <div>
-        @foreach ($articles as $article)
-            @if(auth()->user()->id == $article->user_id)
-            <p>{{ $article->title }} {{ $article->resume }} - {{ $article->text }}</p>
-            <hr>
-            @endif
-        @endforeach
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            {{-- <x-jet-welcome /> --}}
+            <div>
+                <p>Mostrando últimos artigos de {{ Auth::user()->name }}.</p>
+            
+                <input type="text" name="message" id="message" wire:model="message">
+            
+                <hr>
+            
+                
+                <div>
+                    @foreach ($articles as $article)
+                        @if(auth()->user()->id == $article->user_id)
+                        {{-- <p>{{ $article->title }} {{ $article->resume }} - {{ $article->text }}</p> --}}
+                        <h3><strong>{{ $article->title }}</strong></h3>
+                        <h4><i>{{ $article->resume }}</i></h4>
+                        <p>{{ $article->text }}</p>
+                        <a href="#">Atualizar artigo</a>
+                        <a href="#">Remover artigo</a>
+                        <hr>
+                        @endif
+                    @endforeach
+                </div>
+            
+                <div>
+                    {{  $articles->links() }}
+                </div>
+            
+            </div>
+        </div>
     </div>
-
-    <div>
-        {{  $articles->links() }}
-    </div>
-
 </div>
--- resources/views/livewire/show-articles.blade.php
+++ resources/views/livewire/show-articles.blade.php
@@ -8,7 +8,10 @@
     
     <div>
         @foreach ($articles as $article)
-            <p>{{ $article->title }} {{ $article->resume }} - {{ $article->text }}</p>
+            {{-- <p>{{ $article->title }} {{ $article->resume }} - {{ $article->text }}</p> --}}
+            <h3><strong>{{ $article->title }}</strong></h3>
+            <h4><i>{{ $article->resume }}</i></h4>
+            <p>{{ $article->text }}</p>
             <hr>
         @endforeach
     </div>
