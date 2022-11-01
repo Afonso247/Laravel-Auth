769fe8f67c9d92cd2ccbcd95cc8d2fae77dc9a83
Afonso247 <afonso.h2002@gmail.com>
2022-11-01 01:10:57 -0300

    Tentativa de finalizar o CRUD Update :(

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Meus artigos') }}
+                        {{ __('Página de Artigos') }}
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
